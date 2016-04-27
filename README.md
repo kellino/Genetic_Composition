e23b5c19adc65a0bf35eb69995850254c4c5f949
David Kelly <thedavidkelly@hotmail.com>
2016-04-27 09:17:09 +0100

    using python3.5

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 Simple algorithmic composition for Computer Music UCl
 ===========
 
+Requirements:
+    Python3.5
+    numpy
+    pydub (available on pip)
+
+While the program is written in python3.5, it should be relatively easy to convert to python2.7
+
 The sample, for which I am not the copyright holder, is taken from a performance by Andreas Scholl of the
 traditional English Ballad 'I will give my love an apple'
--- gen_music.py
+++ gen_music.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.5
 import logging
 import random
 import numpy as np
@@ -7,6 +8,7 @@ from collections import namedtuple
 from pydub import AudioSegment as AS
 from pydub import playback as pb
 from pydub import effects as ef
+from functools import reduce
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
@@ -62,10 +64,10 @@ class Composer:
 
     def individual(self, words):
         """ generate a randomized member of population """
-        wordlist = [v for k, v in words.iteritems()]
+        wordlist = [v for k, v in words.items()]
         random.shuffle(wordlist)
         individual = []
-        for i in xrange(len(wordlist)):
+        for i in range(len(wordlist)):
             if i % 2 == 0:
                 wordlist[i].section.reverse()
                 individual.append(wordlist[i])
@@ -77,7 +79,7 @@ class Composer:
         return individual
 
     def population(self, words, count):
-        return [self.individual(words) for x in xrange(count)]
+        return [self.individual(words) for x in range(count)]
 
     def get_deviation(self, individual):
         """ deviation of member of population """
@@ -100,22 +102,27 @@ class Composer:
         rand = random.randint(0, 100)
 
         if rand > extinct:
-            # find the member of the population with the best fitness (ie, the one with the smallest
+            # find the member of the population with the best fitness
+            # (ie, the one with the smallest
             # deviation from the target)
             fittest_index = graded.index(min(graded))
             fittest = population[fittest_index]
         else:
-            # the fittest can sometimes die out, through no real fault of its own. Let's factor that in
-            # and assume that the least similar - ie the most genetically diverse - manages to survive
+            # the fittest can sometimes die out, through no real
+            # fault of its own. Let's factor that in
+            # and assume that the least similar - ie the most
+            # genetically diverse - manages to survive
             fittest_index = graded.index(max(graded))
             fittest = population[fittest_index]
 
-        # mutation may or may not occur between generations, and may be benficial, or not...
+        # mutation may or may not occur between generations, and
+        # may be benficial, or not...
         rand = random.randint(0, 100)
 
         if rand < mutate:
-            # individual expects a dictionary, so let's convert our fittest member back to this form
-            words = {i: fittest[i] for i in xrange(0, len(fittest))}
+            # individual expects a dictionary, so let's convert our
+            # fittest member back to this form
+            words = {i: fittest[i] for i in range(0, len(fittest))}
             fittest = self.individual(words)
             # replace the previous fittest with the new fittest
             population[fittest_index] = fittest
@@ -125,7 +132,7 @@ class Composer:
     def markov_chain(self, words, seed=3):
         # add the seed, defaults to apple
         self.composition.append(words[seed].section)
-        for i in xrange(27):
+        for i in range(27):
             randFloat = random.uniform(0, 1)
             temp = sorted(self.markov[seed])
             for j in range(len(temp)):
@@ -137,7 +144,8 @@ class Composer:
             self.composition.append(words[seed].section)
 
     def get_original(self):
-        segment = self.sampler.get_ogg_sample('./I will give my love an apple.ogg')
+        segment = self.sampler.get_ogg_sample(
+            r'./I will give my love an apple.ogg')
         words = self.sampler.split_sample(segment)
         return segment, words
 
@@ -146,7 +154,7 @@ class Composer:
         self.composition.append(original[2000:25000])
 
         rand = random.randint(2, 4)
-        wordlist = [v.section for k, v in words.iteritems()]
+        wordlist = [v.section for k, v in words.items()]
         for i in range(rand):
             for word in wordlist:
                 if i == 0:
@@ -176,14 +184,13 @@ class Composer:
         original, words = self.get_original()
         # generate exposition
         self.introduction(original, words)
-        target = self.get_deviation([x for k, x in words.iteritems()])
+        target = self.get_deviation([x for k, x in words.items()])
         logging.debug("target is {}".format(target))
         # pseudo-development
         self.markov_chain(words)
         # false recapitulation
         self.composition.append(original[2500:16000])
         # start mutation
-        devs = []
         pop = self.population(words, 4)
         self.add_population(pop)
         pop = self.mutate(pop, target)
